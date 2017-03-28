7451bd37f13c588ee730bb03f220f9ae78a25780
Tyson-Rodriguez <tysonrod17@gmail.com>
2017-03-28 06:12:40 -0700

    added all

---
+++ RandomIntUp.js
@@ -0,0 +1,23 @@
+
+// binary random Integer
+// save file as randomInCreate.js
+// to run type "node randomInCreate.js"
+var randomInteger = function() {
+	var ival = Math.random()*26+65;
+	// console.log(ival);
+	ival = Math.floor(ival);
+	return ival;
+}
+// main 
+var i, rInt ;
+var letter = ' ' ;
+var tst = 0;
+for (i = 0; i < 65536; i++){
+rInt	= randomInteger();
+if (rInt == 17)tst++;
+//console.log(rInt+" ");
+letter = parseInt(rInt);
+letter = String.fromCharCode(rInt);
+process.stdout.write(letter);
+}
+console.log("\n\n Count 17 = "+tst);
